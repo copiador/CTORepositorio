a78232316b91087ba2a5fffbc5cf2840dac3e314
jesse <jesse@jesse-PC>
2014-04-10 00:22:07 -0300

    Testando Mandar

--- ProjetoWeb/src/br/com/fafica/cto/exception/CampoVasioExpection.java
+++ ProjetoWeb/src/br/com/fafica/cto/exception/CampoVasioExpection.java
@@ -1,5 +1,14 @@
 package br.com.fafica.cto.exception;
 
-public class CampoVasioExpection {
+public class CampoVasioExpection extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CampoVasioExpection(String campo){
+		super("O campo" + campo + "está vazio");
+	}
 
 }
